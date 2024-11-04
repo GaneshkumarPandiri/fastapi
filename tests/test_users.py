39ce293840166fa6002b971efd53ec4f53978f29
Ganesh Kumar <ganesh.gk818508@gmail.com>
2024-11-04 13:38:27 +0530

    updated tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,40 +1,49 @@
-from app import schemas
 import pytest
 import jwt
+from app import schemas
+
 from app.config import settings
 
 
+# def test_root(client):
 
+#     res = client.get("/")
+#     print(res.json().get('message'))
+#     assert res.json().get('message') == 'Hello World'
+#     assert res.status_code == 200
 
-def test_root(client):
-    resp = client.get("/")
-    assert resp.status_code == 200
-    assert resp.json().get("message") == "Hello World!!!"
 
 def test_create_user(client):
-    #json is for body need to send through body data
-    resp = client.post("/users/",json={"email":"ganesh.m0477@gmail.com","password":"Ganesh@m0477"})
-    new_user = schemas.UserResponse(**resp.json())
-    assert resp.status_code == 201
-    assert new_user.email == "ganesh.m0477@gmail.com"
+    res = client.post(
+        "/users/", json={"email": "ganesh@gmail.com", "password": "password123"})
 
-def test_login_user(test_user,client):
-    #data is for body need to send through form data
-    resp = client.post("/login",data={"username":test_user["email"],"password":test_user["password"]})
-    login_details = schemas.Token(**resp.json())
-    payload = jwt.decode(login_details.access_token,settings.secret_key,algorithms=settings.algorithm)
-    id : str = payload.get("user_id")
-    assert id == test_user["id"]
-    assert resp.status_code == 200
+    new_user = schemas.UserResponse(**res.json())
+    assert new_user.email == "ganesh@gmail.com"
+    assert res.status_code == 201
 
 
-# @pytest.mark.parametrize("email, password, status_code")
-def test_login_wrong_user(test_user,client):
-    #data is for body need to send through form data
-    resp = client.post("/login",data={"username":test_user["email"],"password":"wrongpassword"})
-    assert resp.status_code == 403
-    assert resp.json().get('detail') == "Invalid Credentials"
-    
+def test_login_user(test_user, client):
+    res = client.post(
+        "/login", data={"username": test_user['email'], "password": test_user['password']})
+    login_res = schemas.Token(**res.json())
+    payload = jwt.decode(login_res.access_token,
+                         settings.secret_key, algorithms=[settings.algorithm])
+    id = payload.get("user_id")
+    assert id == test_user['id']
+    assert login_res.token_type == "bearer"
+    assert res.status_code == 200
 
 
+@pytest.mark.parametrize("email, password, status_code", [
+    ('wrongemail@gmail.com', 'password123', 403),
+    ('ganesh@gmail.com', 'wrongpassword', 403),
+    ('wrongemail@gmail.com', 'wrongpassword', 403),
+    (None, 'password123', 422),
+    ('ganesh@gmail.com', None, 422)
+])
+def test_incorrect_login(test_user, client, email, password, status_code):
+    res = client.post(
+        "/login", data={"username": email, "password": password})
 
+    assert res.status_code == status_code
+    # assert res.json().get('detail') == 'Invalid Credentials'
